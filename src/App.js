d3561e0c10b0c551f4c05279c8cb63fc6a4b444c
lisantwi <lisaantwi@gmail.com>
2019-09-18 18:03:24 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import './App.css';
+
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+}
+
+export default App;
